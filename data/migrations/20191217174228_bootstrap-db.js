ac2f4ff069ebbaa9d55d7f34e68861d1a852826b
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-23 16:58:22 +0100

    heroku push

--- data/migrations/20191217174228_bootstrap-db.js
+++ data/migrations/20191217174228_bootstrap-db.js
@@ -5,7 +5,7 @@ exports.up = function (knex) {
       tbl.string("name", 128).notNullable();
     })
     .createTable("users", (tbl) => {
-      tbl.increments();
+      tbl.increments("user_id");
       tbl.string("username", 128).notNullable().unique();
       tbl.string("password", 256).notNullable();
       tbl.integer("role").unsigned().defaultTo(1);
@@ -14,41 +14,43 @@ exports.up = function (knex) {
       //.onUpdate("CASCADE")
       //.defaultTo(2)
     })
-    .createTable('classes', table => {
-      table.increments('class_id')
-      table.string('name').notNullable()
-      table.string('instructor_username')
-      table.string('type')
-      table.string('start_time')
-      table.string('duration')
-      table.string('intensity')
-      table.string('location')
-      table.integer('attendees').defaultTo(0)
-      table.integer('class_size')
-  })
-    .createTable('class_clients', table => {
-      table.increments('class_clients_id')
-      table.integer('class_id')
+    .createTable("classes", (table) => {
+      table.increments("class_id");
+      table.string("name").notNullable();
+      table.string("instructor_username");
+      table.string("type");
+      table.string("start_time");
+      table.string("duration");
+      table.string("intensity");
+      table.string("location");
+      table.integer("attendees").defaultTo(0);
+      table.integer("class_size");
+    })
+    .createTable("class_clients", (table) => {
+      table.increments("class_clients_id");
+      table
+        .integer("class_id")
         .unsigned()
         .notNullable()
-        .references('class_id')
-        .inTable('classes')
-        .onDelete('RESTRICT')
-        .onUpdate('RESTRICT')
-      table.integer('user_id')
-      .unsigned()
-      .notNullable()
-      .references('user_id')
-      .inTable('users')
-      .onDelete('RESTRICT')
-      .onUpdate('RESTRICT')
-  })
+        .references("class_id")
+        .inTable("classes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      table
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    });
 };
 
 exports.down = function (knex) {
   return knex.schema
-  .dropTableIfExists("users")
-  .dropTableIfExists("roles")
-  .dropTableIfExists('class_clients')
-  .dropTableIfExists('classes')
+    .dropTableIfExists("users")
+    .dropTableIfExists("roles")
+    .dropTableIfExists("class_clients")
+    .dropTableIfExists("classes");
 };
